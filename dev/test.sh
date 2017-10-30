52d05f09c731dfbf9bb0d97dd7f83ea3a3a62b27
Katsutoshi Seki <sekika@users.noreply.github.com>
2017-10-30 12:43:51 +0900

    flake log

--- dev/test.sh
+++ dev/test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo 'flake8'
-flake8 ../kaidoku/*.py | grep -v E501
+flake8 ../kaidoku/*.py | sed -e 's/^.*kaidoku\///' > flake8.txt
+grep -v E501 flake8.txt
 echo 'kaidoku test'
 kaidoku test > test.txt
 diff test-pre.txt test.txt > diff.txt
